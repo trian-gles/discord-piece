728b3e19ccd8286022fc456fc772301a9ef9cb30
trian-gles <69212477+trian-gles@users.noreply.github.com>
2021-02-19 13:32:21 -0500

    Began filtering for spanish

--- genres/filter_genres.py
+++ genres/filter_genres.py
@@ -1,4 +1,7 @@
 import json
+import detectlanguage
+
+detectlanguage.configuration.api_key = "KEY"
 
 with open('nationalities.json') as nat_file:
     nats = json.load(nat_file)
@@ -9,16 +12,25 @@ with open('all_genres.json') as gen_file:
 def check_nat(genre):
     for nat in nats:
         if nat.lower() in genre.lower():
-            print(genre + " is a bad idea")
+            print(genre + " is ethnic and a bad idea")
             return True
+
+def check_spanish(genre):
+    try:
+        language = detectlanguage.detect(genre)[0]['language']
+    except IndexError:
+        return False
+    if  language == 'es':
+        print(genre + " is spanish not a good idea")
+        return True
 
 filt_gens = []
 
 for gen in unfilt_gens:
-    if not check_nat(gen):
-        filt_gens.append(gen)
+    if check_spanish(gen) or check_nat(gen):
+        continue
+    filt_gens.append(gen)
 
 
 with open('filt_genres.json', 'w') as filt_file:
     json.dump(filt_gens, filt_file)
-    pass
